e3fafe81801ed5e8c5e6423ed12bf88ecaf3c1fb
akiyoko <akiyoko@users.noreply.github.com>
2021-01-04 00:59:02 +0900

    Fix models

--- shop/models.py
+++ shop/models.py
@@ -11,8 +11,7 @@ class Book(models.Model):
         verbose_name = verbose_name_plural = '本'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(verbose_name='タイトル', max_length=20,
-                             unique=True)
+    title = models.CharField(verbose_name='タイトル', max_length=20)
     price = models.IntegerField(verbose_name='価格', null=True, blank=True)
     created_at = models.DateTimeField(verbose_name='登録日時',
                                       auto_now_add=True)
