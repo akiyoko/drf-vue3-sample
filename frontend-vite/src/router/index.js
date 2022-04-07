fdcedb699587f6a940ad69dcdaafbbe6a8d20a16
akiyoko <akiyoko@users.noreply.github.com>
2022-04-08 04:07:40 +0900

    Fix router

--- frontend-vite/src/router/index.js
+++ frontend-vite/src/router/index.js
@@ -18,16 +18,8 @@ const router = createRouter({
       name: "login",
       component: LoginView,
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
     {
-      path: "/:catchAll(.*)",
+      path: "/:pathMatch(.*)*",
       redirect: "/",
     },
   ],
@@ -44,22 +36,18 @@ router.beforeEach((to) => {
   console.log("to.path=", to.path);
   console.log("isLoggedIn=", isLoggedIn);
 
-  // ログインが必要な画面に遷移しようとした場合
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    // ログインしていない場合
-    if (!isLoggedIn) {
-      console.log("User is not logged in.");
-      // まだ認証用トークンが残っていればユーザー情報を再取得
-      if (token != null) {
-        console.log("Try to renew user info.");
-        authStore.renew().catch(() => {
-          // 再取得できなければログイン画面へ
-          forceToLoginPage(to);
-        });
-      } else {
-        // 認証用トークンが無い場合は、ログイン画面へ
+  // 未ログイン状態でログインが必要な画面に遷移しようとした場合
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    // まだ認証用トークンが残っていればユーザー情報を再取得
+    if (token != null) {
+      console.log("Try to renew user info.");
+      authStore.renew().catch(() => {
+        // 再取得できなければログイン画面へ
         forceToLoginPage(to);
-      }
+      });
+    } else {
+      // 認証用トークンが無い場合は、ログイン画面へ
+      forceToLoginPage(to);
     }
   }
 });
