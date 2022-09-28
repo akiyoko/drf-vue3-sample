cfc79d0182efaec1804670b3d3404bf070d879f2
akiyoko <akiyoko@users.noreply.github.com>
2022-09-29 02:03:06 +0900

    Fix

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -4,11 +4,14 @@ from shop.models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Book
         fields = ['id', 'title', 'price']
         extra_kwargs = {
-            'title': {'error_messages': {'blank': 'タイトルは空にできません。'}},
-            'price': {'error_messages': {'invalid': '価格には有効な整数を入力してください。'}},
+            'title': {'error_messages': {
+                'blank': 'タイトルは空にできません。',
+            }},
+            'price': {'error_messages': {
+                'invalid': '価格には有効な整数を入力してください。',
+            }},
         }
