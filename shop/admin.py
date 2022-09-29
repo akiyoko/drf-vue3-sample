4c761b25debe423991d35089ed9c69ddc27ca7a4
akiyoko <akiyoko@users.noreply.github.com>
2022-09-30 02:18:07 +0900

    yapf適用

--- shop/admin.py
+++ shop/admin.py
@@ -5,7 +5,7 @@ from .models import Book
 
 class BookModelAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'id', 'created_at')
-    ordering = ('-created_at',)
+    ordering = ('-created_at', )
     readonly_fields = ('id', 'created_at')
 
 
