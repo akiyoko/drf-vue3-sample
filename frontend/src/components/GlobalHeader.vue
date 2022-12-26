139878d463fc17435e7a21402991c7b37fea6817
akiyoko <akiyoko@users.noreply.github.com>
2022-12-27 05:22:05 +0900

    scoped

--- frontend/src/components/GlobalHeader.vue
+++ frontend/src/components/GlobalHeader.vue
@@ -75,7 +75,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 button.v-btn {
   text-transform: none;
 }
--- frontend/src/components/GlobalMessage.vue
+++ frontend/src/components/GlobalMessage.vue
@@ -38,7 +38,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #message {
   min-height: 110px;
 }
